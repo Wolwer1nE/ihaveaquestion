db290cb7d6ad9646e774ed0b7192edadae18ea5d
wolwer1ne <wolwerine@yandex.ru>
2019-12-03 10:11:28 +0300

    Fixes images in Chrome

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -5,11 +5,17 @@ class AuthController < ApplicationController
 
   end
 
+  def set_development_user
+    user = User.first
+    session['user'] = user.id
+    redirect_to '/'
+  end
+
   def new
-    p auth_github_url
+    return set_development_user if Rails.env == 'development'
+
     redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_ID']}"+
-                              "&scope=read:user&state=#{SecureRandom.hex}"+
-                              "&redirect_uri=#{auth_github_url}"
+                              "&scope=read:user&state=#{SecureRandom.hex}"
   end
 
   def github
@@ -25,9 +31,7 @@ class AuthController < ApplicationController
   private
 
   def get_user_data
-    return {login: 'wolwer1ne'} if Rails.env == 'development'
-
-    request_params = {client_id: ENV['GITHUB_ID'],
+   request_params = {client_id: ENV['GITHUB_ID'],
                       client_secret: ENV['GITHUB_SECRET'],
                       code: params[:code],
                       state: params[:state]}
