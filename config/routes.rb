d9d6d7c6e1dcb4bf13500e024da2926b0eca171d
wolwer1ne <wolwerine@yandex.ru>
2019-12-12 21:59:16 +0300

    Updates questions list

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+  root to: 'questions#index'
   resources :questions, only: %i[index show new create update]
 
   resources :answers, only: %i[create]
   get '/auth', to: 'auth#index'
   get 'auth/new', to: 'auth#new'
   get 'auth/github', to: 'auth#github'
+
+  get 'home', to: 'home#index'
 end
